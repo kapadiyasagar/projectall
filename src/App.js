e3538d7ab4a3ccd12e1b789491c70e58fedde737
kapadiyasagar <sagarkapadiya220@gmail.com>
2022-05-19 10:04:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
+import Home from './container/Home/Home';
+import { Route ,Switch } from 'react-router-dom';
+import Departmant from './container/Departmant/Departmant';
+import About from './container/About/About';
+import Contact from './container/Contact/Contact';
+import Doctor from './container/Doctor/Doctor';
+
+function App() {
+  return (
+  <>
+  <Header/>
+  <Switch>
+    
+    <Route exact path={"/"} component={Home}/>
+    <Route exact path={"/departments"} component={Departmant}/>
+    <Route exact path={"/doctors"} component={Doctor}/>
+    <Route exact path={"/about"} component={About}/>
+    <Route exact path={"/contact"} component={Contact}/>
+    
+
+  </Switch>
+  <Footer/>
+  </>
+  );
+}
+
+export default App;
+
---
+++ src/container/log-in/Log-in.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Login(props) {
+    return (
+        <div>
+            <Button
+                color="primary"
+            >
+                Click Me
+            </Button>
+        </div>
+    );
+}
+
+export default Login;
